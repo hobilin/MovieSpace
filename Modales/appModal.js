657b78062109d054cc154ea05ffa27a3b75142e3
jotavasquez <jotavasquezjota@gmail.com>
2018-01-10 12:13:35 -0300

    subiendo formatos de modales

---
+++ Modales/appModal.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //Función para activar filtros de búsqueda
+    $(".btn-round").click(function () {
+        $(this).toggleClass("tag-red-active");
+    });
+
+});
