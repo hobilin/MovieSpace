37975bb8bbea1c1f43e9e7c20269b2b22db4454c
hobilin <paulibareyes@gmail.com>
2018-01-10 12:34:05 -0300

    añaniendo función para agregar posters

---
+++ assets/data/filtro.js
@@ -0,0 +1,47 @@
+var filtroArray = [
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'movie',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'movie',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'movie',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'movie',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'movie',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'movie',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+
+
+
+];
---
+++ assets/data/series.js
@@ -0,0 +1,17 @@
+var filtroArray = [
+  {
+    title: 'coco',
+    genre: ['fantasy', 'family', 'comedy'],
+    type: 'series',
+    ratings: '40/100',
+    actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+  },
+  var filtroArray = [
+    {
+      title: 'coco',
+      genre: ['fantasy', 'family', 'comedy'],
+      type: 'series',
+      ratings: '40/100',
+      actors: ['Matt Debergalis', 'pepita', 'momo', 'hehe'],
+    },
+];
